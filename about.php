b57195645a36ea5d807ef42da082d19b7593494d
Denice Shanley <105403621+shandycheeks@users.noreply.github.com>
2023-05-16 08:30:21 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us | The Mind Mechanics</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+	<center>
+		<h1 style="text-indent: 80px;">The Mind Mechanics</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Doctors</a></li>
+				<li class="active"><b><a href="#">About Us</b></a></li>
+				<li><a href="#">Home</a></li>
+			</ul>
+		</nav>
+	</center>
+	</header>
+	<main>
+		<style>
+		.container {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		.image {
+			flex: 1;
+			padding: 20px;
+		}
+		.text {
+			flex: 2;
+			padding: 20px;
+		}
+	</style>
+	
+	
+	<br>
+		<section id="AboutUs">
+		<div class="container">
+		<div class="image">
+			<img src="img/mentalbg.png" alt="bg" width=600">
+		</div>
+		<div class="text">
+		<center>
+			<h2>Mental Healthcare For You, With You</h2>
+			<br>
+		<section id="mission">
+			<h2>Our Mission</h2>
+			<i>“There is no normal life that is free of pain. It’s the very wrestling with our problems that can be the impetus for our growth.” — Fred Rogers</i>
+			<p>Welcome to our mental health appointment website. Our mission is to provide accessible and compassionate mental health services to our community. We understand that seeking help for mental health concerns can be difficult, and we strive to create a safe and welcoming environment for our patients.</p>
+		</section>
+		<section id="team">
+			<h2>Our Team</h2>
+			<p>Our team of licensed and experienced mental health professionals are dedicated to providing personalized care to each patient. We believe in a holistic approach to mental health treatment that takes into account the unique needs and experiences of each individual. Our clinic is equipped with comfortable and private therapy rooms to ensure that our patients feel safe and at ease during their appointments. We offer a wide range of mental health services, including individual therapy, couple’s therapy, family therapy, and group therapy.</p>
+		</section>
+		<section id="confidentiality">
+			<h2>Confidentiality</h2>
+				<p> At our clinic, we prioritize patient confidentiality and respect. We follow strict privacy protocols and maintain a non-judgmental and accepting environment. We also offer flexible scheduling options and tele therapy services to accommodate our patients’ busy lives and unique needs.
+
+                Thank you for considering our clinic for your mental health needs. We are committed to providing you with compassionate and effective care to support your mental health and wellbeing.</p>
+		</section>	
+			</center>
+	</section>
+	<div id="design" style="background-color: #E8D5C4;">
+		<center>
+			<img src="img/design.png" alt="mental health matters" width=300">
+		</center>
+		<section id="services">
+		<center>
+		<h2>Mental Health Services</h2>
+		<p>We offer a wide range of mental health services to support the wellbeing of our patients:</p><br>
+		</center>
+		<ul class="mhs">
+			<p style="text-indent: 50px;">◦ <a href="#"> Individual therapy</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Couple’s therapy</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Family therapy</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Group therapy</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Psychiatric evaluation and medication management</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Specialized therapy for trauma, addiction, and other concerns</a></p>
+			<p style="text-indent: 50px;">◦ <a href="#">Teletherapy services for convenient access to care</a></p><br>
+		</ul>
+	<center>
+		<p>Our team of mental health professionals will work with you to create a personalized treatment plan based on your unique needs and goals. We believe in a collaborative and supportive approach to therapy, and we are committed to helping you achieve lasting positive change in your life.</p><br>
+	<div style="background-color: #FFB84C; padding: 10px 20px; display: inline-block; border-radius: 5px;">
+	<a href="#" style="color: #fff; text-decoration: none; font-weight: bold;">Make an Appointment</a>
+	</a>
+	</center>
+	<br>
+	</div>
+	</div>
+	</section> 
+	<hr>
+	<br>
+	<div id="header">
+  <div id="logo" style="float: left;">
+    <img src="img/logo2.png" alt="mental health matters" height="100">
+  </div>
+  <section id="contact-us" style="float: left;">
+    <h3>Contact Us</h3>
+    <p>If you have any questions or would like to schedule an appointment, please don't hesitate to contact us:</p>
+    <ul>
+      <p><b>Phone: </b><a href="#">(02) 533 7515</a></p>
+      <p><b>Email:</b> <a href="#">info@mindmechanicsph.com</a></p>
+	  <p><b>Address:</b> Don Jesus Blvd. Alabang Hills Barangay Cupang Unit 301 RB Arcade Building, Muntinlupa City, Metro Manila, Philippines 1771</p><br><br>
+	  <p style="color: gray;"><i><u> Denice Shanley C. Alemania</u> | BSIT 2-2N</i></p>
+  </ul>
+  </section>
+  </div>
+</main>
+</body>
+</html>
